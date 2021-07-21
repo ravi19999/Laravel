04ad1483ad32caa68e6efc78efaaca4bcd457cc0
Ravi <dahalravindra@gmail.com>
2021-07-21 11:35:02 +0545

    Initial commit :tada:

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,38 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel='stylesheet' href='/app.css' />
+    <!-- <script src="/app.js"></script> -->
+    <title>My Blog</title>
+</head>
+<body>
+    <article>
+        <h1> <a href="/post">Post 1</a> </h1>
+
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae ex quisquam assumenda voluptates velit quo in similique magnam quae doloremque, ab quod sint delectus fuga ratione neque? Ut, quae animi.
+        </p>
+    </article>
+    
+    <article>
+        <h1><a href="/post">Post 2</a> </h1>
+
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae ex quisquam assumenda voluptates velit quo in similique magnam quae doloremque, ab quod sint delectus fuga ratione neque? Ut, quae animi.
+        </p>
+    </article>
+
+
+    <article>
+        <h1><a href="/post">Post 3</a> </h1>
+
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae ex quisquam assumenda voluptates velit quo in similique magnam quae doloremque, ab quod sint delectus fuga ratione neque? Ut, quae animi.
+        </p>
+    </article>
+</body>
+</html>
