84575b1cb462048515fcb1fda2248089c94bb5be
Ravi <dahalravindra@gmail.com>
2021-07-23 13:04:29 +0545

    Seeded the database with fake data

--- database/migrations/2021_07_23_041838_create_categories_table.php
+++ database/migrations/2021_07_23_041838_create_categories_table.php
@@ -16,8 +16,8 @@ class CreateCategoriesTable extends Migration
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
-            $table->string('name');
-            $table->string('slug');
+            $table->string('name')->unique();
+            $table->string('slug')->unique();
         });
     }
 
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\Category;
+use App\Models\User;
+use App\Models\Post;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +16,52 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        User::truncate();
+        Category::truncate();
+        Post::truncate();
+
+        $user =  User::factory()->create();
+
+        $personal = Category::create([
+            'name' => 'Personal',
+            'slug' => 'personal'
+        ]);
+
+        $family = Category::create([
+            'name' => 'Family',
+            'slug' => 'family'
+        ]);
+
+        $work = Category::create([
+            'name' => 'Work',
+            'slug' => 'work'
+        ]);
+
+        Post::create([
+            'user_id' => $user->id,
+            'category_id' => $family->id,
+            'title' => 'My Family Post',
+            'slug' => 'my_work_post',
+            'excerpt' => 'Lorem ipsum dolor sit amet',
+            'body' => 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vel vero ex nam voluptatem aspernatur aliquam iusto, maxime laborum quos sit molestiae sapiente deleniti dolor? A incidunt veniam ut suscipit? Ipsum?'
+        ]);
+
+        Post::create([
+            'user_id' => $user->id,
+            'category_id' => $personal->id,
+            'title' => 'My Work Post',
+            'slug' => 'my_personal_post',
+            'excerpt' => 'Lorem ipsum dolor sit amet',
+            'body' => 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vel vero ex nam voluptatem aspernatur aliquam iusto, maxime laborum quos sit molestiae sapiente deleniti dolor? A incidunt veniam ut suscipit? Ipsum?'
+        ]);
+
+        Post::create([
+            'user_id' => $user->id,
+            'category_id' => $work->id,
+            'title' => 'My Personal Post',
+            'slug' => 'my_family_post',
+            'excerpt' => 'Lorem ipsum dolor sit amet',
+            'body' => 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vel vero ex nam voluptatem aspernatur aliquam iusto, maxime laborum quos sit molestiae sapiente deleniti dolor? A incidunt veniam ut suscipit? Ipsum?'
+        ]);
     }
 }
