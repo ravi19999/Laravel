2f31c7ef45ad9623675776e8cd124411a138815c
Ravi <dahalravindra@gmail.com>
2021-07-23 11:54:25 +0545

    Added route for categories

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -3,7 +3,7 @@
         <h1>{!! $post->title !!}</h1>
     </article>
     <p>
-        <a href="#">{{$post -> category->name}}</a>
+        <a href="/categories/{{ $post->category->slug }}">{{$post -> category->name}}</a>
     </p>
     <div>
         <p>
